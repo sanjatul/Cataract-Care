5c095c53e6a0262b1d7f1e4b53851d5f11db03f8
SanjatulHasanSiam <sanjatulhasansiam.iit@gmail.com>
2024-04-23 23:02:04 +0600

    Add project files.

---
+++ Cataract Care/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cataract_Care.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
